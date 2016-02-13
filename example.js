7cd5017661d023438a1928c76bc9ca22422560f3
Thomas Champagne <th.champagne@gmail.com>
2016-02-13 01:15:50 +0100

    Typo

--- example.js
+++ example.js
@@ -1,15 +1,15 @@
 var XORCrypt = require('./XORCrypt');
 
-var symetricKey = '5_Gk>V!q9umG-dx4GK*V;j!_';
+var symmetricKey = '5_Gk>V!q9umG-dx4GK*V;j!_';
 
 var plainText = 'I have a secret for you !';
 
 console.log('Plain Text: ' + plainText);
 
-var cypherText = XORCrypt.encode(plainText, symetricKey);
+var cypherText = XORCrypt.encode(plainText, symmetricKey);
 
 console.log('Encoded: ' + cypherText);
 
-var decodedCypher = XORCrypt.decode(cypherText, symetricKey);
+var decodedCypher = XORCrypt.decode(cypherText, symmetricKey);
 
 console.log('Decoded: ' + decodedCypher);
